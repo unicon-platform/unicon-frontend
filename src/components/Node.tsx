d0a73641ec23c9b578a234ed323d54032f96a059
Shen Yi Hong <shen_yi_hong@outlook.com>
2024-08-22 20:42:19 +0800

    feat: aesthetics changes ✨

--- src/components/Node.tsx
+++ src/components/Node.tsx
@@ -12,9 +12,9 @@ import { ArtifactNode } from "./ArtifactNode";
 
 export function Node({ data }: FlowNodeProps<FlowNode<NodeData>>) {
   return (
-    <div className="min-w-48 flex flex-col rounded outline outline-1 outline-slate-300 text-slate-300">
+    <div className="min-w-48 flex flex-col rounded outline outline-[0.08rem] outline-neutral-500 font-mono text-slate-300 pb-2 bg-[#141414]">
       {/* Node header */}
-      <div className="flex items-center py-1 px-2 rounded-t tracking-tight text-sm font-mono font-medium uppercase">
+      <div className="flex items-center py-1 px-2 mb-2 rounded-t text-sm font-medium uppercase">
         <GoDotFill
           style={{ color: `${NodeColorMap[data.type]}` }}
           className="mr-1"
@@ -22,7 +22,7 @@ export function Node({ data }: FlowNodeProps<FlowNode<NodeData>>) {
         {data.label}
       </div>
       {/* Node body */}
-      <div className="text-xs text-slate-300 font-mono">
+      <div className="text-xs font-light">
         {data.type === NodeType.USER ? (
           <UserNode course={data.course} />
         ) : data.type === NodeType.GROUP ? (
@@ -30,7 +30,7 @@ export function Node({ data }: FlowNodeProps<FlowNode<NodeData>>) {
         ) : data.type === NodeType.TASK ? (
           <TaskNode />
         ) : data.type === NodeType.ARTIFACT ? (
-          <ArtifactNode fileType={data.fileType}/>
+          <ArtifactNode fileType={data.fileType} />
         ) : (
           <></>
         )}
