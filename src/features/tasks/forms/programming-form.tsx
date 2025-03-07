ce0867c1aff63f6467f97d18474c2c026d7467ef
seeleng <seelengxd@gmail.com>
2025-03-07 22:11:44 +0800

    fix: data_type for shared input

--- src/features/tasks/forms/programming-form.tsx
+++ src/features/tasks/forms/programming-form.tsx
@@ -114,7 +114,7 @@ const ProgrammingForm: React.FC<OwnProps> = ({ title, initialValue, onSubmit }) 
         // Shared user input step for all testcases
         const newSharedUserInputStep: InputStep = {
           ...DEFAULT_USER_INPUT_STEP,
-          outputs: fromValue.map((input) => ({ ...input, type: "DATA", public: true })),
+          outputs: fromValue.map((input) => ({ ...input, type: "DATA", public: true, data_type: "UniconFile" })),
         };
         const testcases = toValue.map((testcase) => ({
           ...testcase,
