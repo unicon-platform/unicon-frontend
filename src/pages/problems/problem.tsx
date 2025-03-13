727e49fc744b31fe6ac3033a09bec61f0085a493
see leng <34371483+seelengxd@users.noreply.github.com>
2025-03-13 19:59:00 +0800

    fix: missing w-full in problem (#122)

--- src/pages/problems/problem.tsx
+++ src/pages/problems/problem.tsx
@@ -72,7 +72,7 @@ const Problem = ({ id, submissionId, submissionAttempts, submittedAt }: ProblemP
   };
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex w-full flex-col gap-8">
       <div className="flex flex-wrap items-center justify-between gap-4">
         <h1 className="flex items-center gap-4 text-3xl font-medium">
           <span>
