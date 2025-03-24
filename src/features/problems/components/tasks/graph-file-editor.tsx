a9441fb76181259f6cba19077954a1a22f4f73ee
seeleng <seelengxd@gmail.com>
2025-03-25 02:18:47 +0800

    fix: disable edit file in graph editor
    - workaround since it will get overwritten by task files

--- src/features/problems/components/tasks/graph-file-editor.tsx
+++ src/features/problems/components/tasks/graph-file-editor.tsx
@@ -8,11 +8,10 @@ import {
   GraphContext,
   GraphDispatchContext,
 } from "@/features/problems/components/tasks/graph-context";
-import { isRequiredInputStep } from "@/lib/compute-graph";
 import { isUniconFile } from "@/lib/utils";
 
 const GraphFileEditor = () => {
-  const { selectedStepId, selectedSocketId, edit, steps } = useContext(GraphContext)!;
+  const { selectedStepId, selectedSocketId, steps } = useContext(GraphContext)!;
 
   const dispatch = useContext(GraphDispatchContext)!;
 
@@ -44,7 +43,6 @@ const GraphFileEditor = () => {
   if (!isUniconFile(selectedSocket.data)) return null;
 
   const file = selectedSocket.data;
-  const isUserInput = isRequiredInputStep(selectedStep);
 
   return (
     <FileEditor
@@ -54,8 +52,9 @@ const GraphFileEditor = () => {
       onFileNameChange={updateFileName}
       onFileContentChange={updateFileContent}
       onFileClosed={() => dispatch({ type: GraphActionType.DeselectSocket })}
-      canEditFileName={edit && !isUserInput}
-      canEditFileContent={edit && !isUserInput}
+      // We disallow editing because it would be overwritten when files are edited in the file tree.
+      canEditFileName={false}
+      canEditFileContent={false}
     />
   );
 };
