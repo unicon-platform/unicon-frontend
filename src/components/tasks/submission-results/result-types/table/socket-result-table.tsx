aa93703a94a9da131d99d3459654188028df7d34
seeleng <seelengxd@gmail.com>
2025-04-21 01:01:51 +0800

    fix: weird \n rendering in task results

--- src/components/tasks/submission-results/result-types/table/socket-result-table.tsx
+++ src/components/tasks/submission-results/result-types/table/socket-result-table.tsx
@@ -8,6 +8,7 @@ type OwnProps = {
 const SocketResultTable: React.FC<OwnProps> = ({ data }) => {
   data.forEach((result) => {
     result.className = result.correct ? "bg-green-800/50 hover:bg-green-800/75" : "bg-red-800/50 hover:bg-red-800/75";
+    result.value = typeof result.value === "string" ? result.value.replace(/\n/g, "\\n") : result.value;
   });
   return <DataTable columns={columns} data={data} hidePagination />;
 };
