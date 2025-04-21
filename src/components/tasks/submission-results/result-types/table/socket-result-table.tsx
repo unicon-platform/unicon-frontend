ac141ea7a8925f3853f40b4a75540245720cec78
Muhammad Rizki Aulia Rahman Maulana <rizki@rizkiarm.com>
2025-04-22 04:06:22 +0800

    feat: render JSON result as grid

--- src/components/tasks/submission-results/result-types/table/socket-result-table.tsx
+++ src/components/tasks/submission-results/result-types/table/socket-result-table.tsx
@@ -7,7 +7,7 @@ type OwnProps = {
 
 const SocketResultTable: React.FC<OwnProps> = ({ data }) => {
   data.forEach((result) => {
-    result.className = result.correct ? "bg-green-800/50 hover:bg-green-800/75" : "bg-red-800/50 hover:bg-red-800/75";
+    result.className = result.correct ? "bg-green-800/30 hover:bg-green-800/0" : "bg-red-800/30 hover:bg-red-800/0";
   });
   return <DataTable columns={columns} data={data} hidePagination />;
 };
