86074b2da123ae57328b3e14d2af9c6312fed77a
Muhammad Rizki Aulia Rahman Maulana <rizki@rizkiarm.com>
2025-04-24 02:12:38 +0800

    Render JSON result as grid (#158)
    * feat: render JSON result as grid
    
    * fix: broken lock file
    
    * refactor(json-grid): use theme and customTheme for styling

---
+++ src/components/tasks/submission-results/result-types/table/output-renderer.tsx
@@ -0,0 +1,59 @@
+// @ts-expect-error package doesn't have type definitions
+import JSONGrid from "@redheadphone/react-json-grid";
+import { ChevronsUpDown } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
+
+/**
+ * Source: https://stackoverflow.com/a/20392392
+ * This function will return `false` for any valid json primitive
+ */
+const tryParseJSONObject = (jsonString: string) => {
+  try {
+    const o = JSON.parse(jsonString);
+    if (o && (o instanceof Array || o instanceof Object)) {
+      return o;
+    }
+  } catch (e) {} // eslint-disable-line
+
+  return false;
+};
+
+type OutputRendererProps = {
+  output: string;
+};
+
+export const OutputRenderer: React.FC<OutputRendererProps> = ({ output }) => {
+  output = output.replace(/\\n/g, "\n");
+  const outputObject = tryParseJSONObject(output);
+
+  if (outputObject) {
+    return (
+      <Collapsible>
+        <JSONGrid
+          data={outputObject}
+          theme={"default"}
+          customTheme={{
+            bgColor: "transparent",
+            tableHeaderBgColor: "transparent",
+            selectHighlightBgColor: "#ffffff1a",
+          }}
+        />
+        <CollapsibleTrigger asChild>
+          <Button variant="ghost" size="sm" className="w-full justify-between">
+            Original Output
+            <ChevronsUpDown className="h-4 w-4" />
+            <span className="sr-only">Toggle</span>
+          </Button>
+        </CollapsibleTrigger>
+        <CollapsibleContent className="space-y-2">
+          <div className="w-full rounded-md border p-2 font-mono text-sm shadow-sm">
+            <p className="whitespace-pre-wrap font-mono">{output}</p>
+          </div>
+        </CollapsibleContent>
+      </Collapsible>
+    );
+  }
+  return <p className="whitespace-pre-wrap font-mono">{output}</p>;
+};
